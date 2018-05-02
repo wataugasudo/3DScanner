653e3ff33547b250777d74a268ba4109e160fcf1
benbroce3 <benbroce2@gmail.com>
2018-05-02 03:08:24 -0400

    Strip dependencies to essentials & attribution

--- code/firmware/v2/libs/LiquidMenu/doc/Doxygen/html/search/all_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['i2c',['I2C',['../_liquid_menu__config_8h.html#a457a9aa93dbb216459873a30bdb4d84a',1,'LiquidMenu_config.h']]],
-  ['init',['init',['../class_liquid_menu.html#ac1afbdecb999988469894a8afd14f100',1,'LiquidMenu']]]
-];
--- code/firmware/v2/libs/LiquidMenu/doc/Doxygen/html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['init',['init',['../class_liquid_menu.html#ac1afbdecb999988469894a8afd14f100',1,'LiquidMenu']]]
-];
